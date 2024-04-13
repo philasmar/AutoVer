ac769f761ba4e39c0f21a75388b71bdabe55f10f
Phil Asmar <phil.asmar@gmail.com>
2024-04-12 20:47:31 -0400

    chore: fix path issue on windows

--- src/AutoVer/Services/IO/PathManager.cs
+++ src/AutoVer/Services/IO/PathManager.cs
@@ -2,5 +2,9 @@ namespace AutoVer.Services.IO;
 
 public class PathManager : IPathManager
 {
+    public char DirectorySeparatorChar => Path.DirectorySeparatorChar;
+
     public string Combine(params string[] paths) => Path.Combine(paths);
+
+    public string? GetExtension(string? path) => Path.GetExtension(path);
 }
